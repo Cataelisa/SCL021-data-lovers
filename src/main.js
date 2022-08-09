f31c2f74cf1d90085bf363331c9a3a4ce70da3a2
cataelisa <catalinamoralesmedina@gmail.com>
2022-08-09 09:53:56 -0400

    listos menu de navegacion y aside

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
+import data from './data/harrypotter/data.js';
+
 
 console.log(example, data);
+
+
+function goToCharacters() {
+    // show the  characters
+    let characters = document.getElementById('characters');
+    characters.style.display = 'block'
+
+    // Hide the front page
+    let frontPage = document.getElementById('front-page');
+    frontPage.style.display = 'none'
+  }
+
+  //go to characters 
+  let  showCharacters= document.getElementById('personajes');
+  showCharacters.addEventListener('click', goToCharacters); 
+
+
+
+
