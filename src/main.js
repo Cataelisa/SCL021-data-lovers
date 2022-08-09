cde178265900e55566bb50b5232710345486eb0a
AshleyBustos <ashley.bustos.e@gmail.com>
2022-08-09 10:07:39 -0400

    Pagina de inicio responsive y conectada a segundo div

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import { example } from './data.js';
+//import { example } from "./data.js";
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from "./data/pokemon/pokemon.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+function goToCharacters() {
+  // show the  characters
+  let characters = document.getElementById("characters");
+  characters.style.display = "block";
+
+  // Hide the front page
+  let frontPage = document.getElementById("front-page");
+  frontPage.style.display = "none";
+}
+
+//go to characters
+let showCharacters = document.getElementById("personajes");
+showCharacters.addEventListener("click", goToCharacters);
+
+//console.log(example, data);
